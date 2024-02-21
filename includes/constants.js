e177fbbec9e81c634e8933f240487a00564cd578
Christopher Manzano <mvca1507@gmail.com>
2024-02-21 16:05:18 +0000

    Added experiments section

---
+++ includes/constants.js
@@ -0,0 +1,17 @@
+/*
+This module contains user defined constants used in the project
+*/
+
+// Data split
+const TRAINING_MONTHS = 12;
+const TESTING_MONTHS = 3;
+const VALIDATION_MONTHS = 3;
+// Compute the intervals to filter by.
+const TESTING_START_MONTHS = TESTING_MONTHS;
+const VALIDATION_START_MONTHS = TESTING_START_MONTHS + VALIDATION_MONTHS;
+const TRAINING_START_MONTHS = VALIDATION_START_MONTHS + TRAINING_MONTHS;
+
+// Model
+const MODEL = "availability";
+
+module.exports = {TESTING_START_MONTHS, VALIDATION_START_MONTHS, TRAINING_START_MONTHS, MODEL};
